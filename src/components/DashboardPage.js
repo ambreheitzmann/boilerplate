6397864d12744abb54c73be1bd69a4ccf16dd02f
Ambre Heitzmann <am.heitzmann@gmail.com>
2018-11-14 19:21:50 +0100

    Initial commit

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DashboardPage = () => (
+  <div>
+   
+  </div>
+);
+
+export default DashboardPage;
---
+++ src/reducers/auth.js
@@ -0,0 +1,14 @@
+// Expenses Reducer
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return {
+                uid: action.uid
+            }
+        case 'LOGOUT':
+            return {}
+        default:
+            return state;
+    }
+};
